16270b8b9d99379a12820443641222b124bfe843
jiisloth <joonas.val@gmail.com>
2017-01-26 05:30:15 +0200

    player initialization done

--- script.js
+++ script.js
@@ -7,27 +7,60 @@ var maps = ["LuigiCircuit", "PeachBeach", "BabyPark", "DryDryDesert", "MushroomB
 
 var names = ["AAAAAA", "BBBB", "CCCCCC", "DDDDDD", "EEEEE", "FFFFF", "GGGGG", "HHHHHHH", "IIIIII", "jsloth"]
 
-var rounds = 12;
+var icons = ["mario", "luigi", "bowser", "peach", "daisy", "yoshi", "toad", "toadette", "donkeykong", "boo", "koopa", "klunk", "chuck", "link", "chiken", "bubble", "mage", "megaman", "pikachu", "samus", "sonic"]
 
-$(document).ready(function () {
+var players = [];
+var playerCount = 0;
+var iconChoice = "";
 
 
-    for (var i = 0; i < maps.length; i++) {
-        map = $('<div class="map"><img class="mapImage" src="images/maps/' + maps[i] + '.png"><div class="mapCount">10</div></div>');
+var rounds = 24;
+
+var i;
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+
+
+function nextPlayer(){
+    $("#nameInput").val("")
+    iconChoice = "";
+}
+
+function initScoreboard() {
+    for (i = 0; i < maps.length; i++) {
+        map = $('<div class="map"><img class="mapImage" src="images/maps/' + maps[i] + '.png"><div class="mapCount">0</div></div>');
         cup = "#cup" + (Math.floor(i / 4) + 1);
         map.appendTo($(cup));
     }
     $("#container").height($(window).height() - ($("#top").height() + $("#bottom").height()));
 
 
-    for (var i = 0; i < names.length; i++) {
-        playerslot = $('<div id="player' + i + '" class="playerSlot"><div class="name">' + names[i] + '</div><div class="score"><div class="playerImageHolder"><img class="playerImage" src="images/playerIcons/mario.png"></div></div></div>')
+    for (i = 0; i < players.length; i++) {
+        playerslot = $('<div id="player' + i + '" class="playerSlot"><div class="name">' + players[i][0] + '</div><div class="score"><div class="playerImageHolder"><img class="playerImage" src="images/playerIcons/' + players[i][1] + '.png"></div></div></div>')
         playerslot.appendTo($("#container"));
     }
 
-    $(".playerSlot").height($("#container").height() / (names.length + 1.1));
+    $(".playerSlot").height($("#container").height() / (players.length + 1.1));
 
-    for (var i = 0; i <= rounds; i++) {
+    for (i = 0; i <= rounds; i++) {
         if (i % 2 == 0) {
             color = "transparent"
         } else {
@@ -46,11 +79,62 @@ $(document).ready(function () {
         'right':'-' + ($(".numberSlot").width() * 1 - 5) + 'px',
         'left': 'auto'
     });
+}
+
+
+$(document).ready(function () {
+    
+    $("#getGameSettings").click( function () {
+        rounds = parseInt($("#roundInput").val());
+        playerCount = $("#playerInput").val();
+        $("#gameSettings").hide();
+        $("#playerSettings").show();
+        nextPlayer()
+
+    });
+    
+    for (i = 0; i < icons.length; i++){
+        iconButton = $('<img id="' + icons[i] + 'Button" class="iconChoice" src="images/playerIcons/'+ icons[i] + '.png">');
+        if (i % 5 == 0){
+            $('<br>').appendTo($("#iconHolder"));
+        }
+        iconButton.appendTo($("#iconHolder"));
+    }
+
+    $(".iconChoice").click( function () {
+        $(".iconChoice").css('border-width', '0');
+        $(this).css('border', '3px solid black');
+        iconChoice = $(this).attr('id').replace("Button", "");
+    });
+
+    $("#getPlayerSettings").click( function () {
+        if (iconChoice == "" && $("#nameInput").val() == "jsloth") {
+            iconChoice = "misato";
+        }
+
+        if (iconChoice != "" && $("#nameInput").val() != ""){
+            var player = [$("#nameInput").val(), iconChoice, 0];
+            $("#" + iconChoice + "Button").remove();
+
+            players.push(player);
+            nextPlayer();
+            if (players.length >= playerCount) {
+                players = shuffle(players);
+                $("#settings").hide();
+                $("#scoreboard").show();
+                initScoreboard();
+
+            }
+
+
+        }
+    });
+
 
     $(window).resize(function () {
         $("#container").height($(window).height() - ($("#top").height() + $("#bottom").height()));
         $(".numberSlot").css("line-height", $(".playerSlot").height() + "px").height($(window).height() - ($("#top").height() + $("#bottom").height()));
-        $(".playerSlot").height($("#container").height() / (names.length + 1.1));
+        $(".playerSlot").height($("#container").height() / (players.length + 1.1));
         $(".playerImage").css({
             'right':'-' + ($(".numberSlot").width() * 1 - 5) + 'px',
             'left': 'auto'
